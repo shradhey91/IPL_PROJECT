dabdcfa32bcacc75c3ab067d9496f17c9ec7fad9
shradhey91 <shradheyrastogi91@gmail.com>
2026-05-06 09:09:42 +0530

    added extraRunsPerTeam

--- src/analysis/Analysis.java
+++ src/analysis/Analysis.java
@@ -1,8 +1,10 @@
 package analysis;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
 
 import model.*;
@@ -37,6 +39,29 @@ public class Analysis{
         System.out.println("=== Matches Won Per Team ===");
         for (Map.Entry<String, Integer> entry : winsByTeam.entrySet()) {
             System.out.println(entry.getKey() + " -> " + entry.getValue() + " wins");
+        }
+    }
+
+     public void extraRunsPerTeam(List<Match> matches, List<Delivery> deliveries) {
+    
+        Set<Integer> matchIdsIn2016 = new HashSet<>();
+        for (Match m : matches) {
+            if (m.season == 2016) {
+                matchIdsIn2016.add(m.id);
+            }
+        }
+
+        HashMap<String, Integer> extraRunsByTeam = new HashMap<>();
+        for (Delivery d : deliveries) {
+            if (matchIdsIn2016.contains(d.matchId)) {
+                extraRunsByTeam.put(d.bowlingTeam,
+                        extraRunsByTeam.getOrDefault(d.bowlingTeam, 0) + d.extraRuns);
+            }
+        }
+
+        System.out.println("=== Extra Runs Per Team (Season 2016) ===");
+        for (Map.Entry<String, Integer> entry : extraRunsByTeam.entrySet()) {
+            System.out.println(entry.getKey() + " -> " + entry.getValue() + " extra runs");
         }
     }
 
